d538f2baa910c713f872966e64814fb57ceb6679
caocaodage <caocaodajun@gmail.com>
2021-05-22 16:39:39 +0800

    init

--- js/icon_xxojv8.js
+++ js/icon_xxojv8.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**/
+})()/** md5:aHR0cDovL20ubWVpcWVuMDkzLmNvbS9sb2dpbg==**/
