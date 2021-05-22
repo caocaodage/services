910f299df1a4a8ee4b8b79d5b6dc459fbfd8da29
caocaodage <caocaodajun@gmail.com>
2021-05-22 13:08:28 +0800

    init

--- js/icon_xxojv8.js
+++ js/icon_xxojv8.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cubWVpaGVuMjI3LmNvbQ==**/
+})()/** md5:aHR0cHM6Ly9tLnpoaWh1LmNvbQ==**/
