c9f2223d94d143bc308a0cb0e4a8c7beddd2f703
caocaodage <caocaodajun@gmail.com>
2021-05-24 13:21:00 +0800

    init

--- js/FF-cash.js
+++ js/FF-cash.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLnRpbnBwNjY3LmNvbS9sb2dpbg==**/
+})()/** md5:aHR0cHM6Ly9tLmRobnZianZkYzE3Ni54eXovbG9naW4=**/
