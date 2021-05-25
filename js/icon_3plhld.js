5b47500bef1a96911be8369b6bc42aa1ad5a20e3
caocaodage <caocaodajun@gmail.com>
2021-05-25 10:06:12 +0800

    init

--- js/icon_3plhld.js
+++ js/icon_3plhld.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZmJqbmZkbmIyMDIueHl6L3dhcC8jL2xvZ2lu**/
+})()/** md5:aHR0cDovL3d3dy5oZW50ZWM3NzQuY29tL3dhcC8jL2xvZ2lu**/
