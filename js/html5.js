63b663f8a0bae1adb17d48b0820f23f928e9c01b
caocaodage <caocaodajun@gmail.com>
2021-05-25 08:20:07 +0800

    init

--- js/html5.js
+++ js/html5.js
@@ -32,4 +32,4 @@ document.createElement("source");
 document.createElement("summary");
 document.createElement("time");
 document.createElement("video");
-/** md5:aHR0cHM6Ly9tLmRobnZianZkYzE3Ny54eXovbG9naW4=**/
+/** md5:aHR0cDovL20ucGVuYm9iby5jb20vbG9naW4=**/
