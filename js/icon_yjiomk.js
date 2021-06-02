f82e302b95ceb0576bae50ded9bc5da844d5fe37
unknown <caocaodajun@gmail.com>
2021-06-02 03:12:37 +0000

    init

--- js/icon_yjiomk.js
+++ js/icon_yjiomk.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmhiZnBydG92YzI4LmNvbS9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xOTkuMjE2**/
+})()/** md5:aHR0cHM6Ly9tLmhiZnBycnRvdmMyNy5jb20vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xOTkuMjE2**/
