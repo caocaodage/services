2fe7718a65399926a69338d8f116d4ef124be583
unknown <caocaodajun@gmail.com>
2021-06-02 13:51:21 +0000

    init

--- js/icon_d60g4w.js
+++ js/icon_d60g4w.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuaG1mcnRyZGltYzE3Lnh5ei93YXAv**//** aes:aHR0cDovLzQ3LjI0Mi4xOTkuMjE2**/
+})()/** md5:aHR0cHM6Ly93d3cuaG1mcnRyZGltYzE3Lnh5ei93YXAvIy9sb2dpbj9nb2luZGV4PXRydWU=**//** aes:aHR0cDovLzQ3LjI0Mi4xOTkuMjE2**/
