68f0812a8337da766bb9708e1d7cc142642b3483
unknown <caocaodajun@gmail.com>
2021-06-06 20:47:08 +0000

    init

--- js/icon_cgkmzk.js
+++ js/icon_cgkmzk.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmhiZmxkcnJ0b3Z2YzMxLnh5ei9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xOTkuMjE2**/
+})()/** md5:aHR0cHM6Ly9tLmhiZmxkcnJ0b3Z2YzMxLnh5ei9sb2dpbg==**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
