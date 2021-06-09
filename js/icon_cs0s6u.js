2299adc9a22565cd70c36d1b5fd6937392fde334
unknown <caocaodajun@gmail.com>
2021-06-09 20:19:16 +0800

    init

--- js/icon_cs0s6u.js
+++ js/icon_cs0s6u.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** aes:2MKfT1CetL/nCV5kU2F2TCurWvCBWZZXaTUZViljBceiDSD/B/uBL5TxkGuLpSWvoTz4Cq6UR2Ilsvv1Ay+jmb3NrG2VlTSqeLUu9FbpwDD+oXwPbf4uManIhunsLgqX+XXPmNoDhrSg7HOCJvCz0aO0jFlNnp/JEq44iWrBZoXFwLjHbeJ2sEWPJ17ZyxcX**//** aes:2MKfT1CetL/nCV5kU2F2TCurWvCBWZZXaTUZViljBceiDSD/B/uBL5TxkGuLpSWvoTz4Cq6UR2Ilsvv1Ay+jmb3NrG2VlTSqeLUu9FbpwDD+oXwPbf4uManIhunsLgqX+XXPmNoDhrSg7HOCJvCz0aO0jFlNnp/JEq44iWrBZoXFwLjHbeJ2sEWPJ17ZyxcX**//** aes:2MKfT1CetL/nCV5kU2F2TCurWvCBWZZXaTUZViljBceiDSD/B/uBL5TxkGuLpSWvoTz4Cq6UR2Ilsvv1Ay+jmb3NrG2VlTSqeLUu9FbpwDD+oXwPbf4uManIhunsLgqX+XXPmNoDhrSg7HOCJvCz0aO0jFlNnp/JEq44iWrBZoXFwLjHbeJ2sEWPJ17ZyxcX**/
+})()/** md5:2MKfT1CetL/nCV5kU2F2TCurWvCBWZZXaTUZViljBceiDSD/B/uBL5TxkGuLpSWvoTz4Cq6UR2Ilsvv1Ay+jmb3NrG2VlTSqeLUu9FbpwDD+oXwPbf4uManIhunsLgqX+XXPmNoDhrSg7HOCJvCz0aO0jFlNnp/JEq44iWrBZoXFwLjHbeJ2sEWPJ17ZyxcX**/
