3c6faf099ee4c914576a74dc680446b098ef8218
unknown <caocaodajun@gmail.com>
2021-06-14 14:28:09 +0800

    init

--- js/icon_pw8lws.js
+++ js/icon_pw8lws.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9maW5hbmNlLnNpbmEuY29tLmNuL3N0b2NrLw==**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
+})()/** md5:aHR0cHM6Ly93d3cuaGJmbHJyZHRvdnZjNTQueHl6L3dhcC8jL2xvZ2lu**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
