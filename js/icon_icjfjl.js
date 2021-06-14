4c8514c70a90d853c073d6c8d661b9c0c43a758a
unknown <caocaodajun@gmail.com>
2021-06-14 13:02:42 +0800

    init

--- js/icon_icjfjl.js
+++ js/icon_icjfjl.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuaGJmbHJyYnRvdnZjNTMueHl6L3dhcC8jL2xvZ2lu**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
+})()/** md5:aHR0cHM6Ly93d3cuaGJmbHJyeHRvdnZjNTYueHl6L3dhcC8jL2xvZ2lu**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
