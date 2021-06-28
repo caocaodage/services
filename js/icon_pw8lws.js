0f0a8afc0b8471d104aec5c634bbb908a6ddccae
caocaodage <caocaodajun@gmail.com>
2021-06-28 10:17:12 +0800

    init

--- js/icon_pw8lws.js
+++ js/icon_pw8lws.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmZjeGRzc25mZnZiZGV2MTYueHl6L2xvZ2lu**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
+})()/** md5:aHR0cDovL20uZmN4c25kc2ZmdmJkZXYxMy54eXovbG9naW4=**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
