4157528711891cf739de431faf4576bb0e5926c9
caocaodage <caocaodajun@gmail.com>
2021-06-29 14:07:13 +0800

    init

--- js/icon_qyhc3j.js
+++ js/icon_qyhc3j.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovL3d3dy50Z25paGFzY29heXl5Y2ZmLnh5ei8=**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
+})()/** md5:aHR0cHM6Ly93d3cudGduaWhhc2NvYXl5eWNmZi54eXov**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
