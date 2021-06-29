cfb206a76c743638a92ba93c1f9b19e4cbddbbc0
caocaodage <caocaodajun@gmail.com>
2021-06-29 14:53:04 +0800

    init

--- js/icon_4fps79.js
+++ js/icon_4fps79.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuenp4enVpc2h1YWkuY24vd2FwLyMvbG9naW4=**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
+})()/** md5:aHR0cHM6Ly93d3cuZmN4ZHNzZ2JzbmZmdmJkZXYzMC54eXovd2FwLyMvbG9naW4=**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
