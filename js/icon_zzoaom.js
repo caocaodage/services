5eb69294cc87ee9afb3da870dd163b54b110cc38
caocaodage <caocaodajun@gmail.com>
2021-08-05 23:18:18 +0800

    init

--- js/icon_zzoaom.js
+++ js/icon_zzoaom.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cud21oeXFmLnh5ei93YXAvIy9sb2dpbg==**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
+})()/** md5:aHR0cHM6Ly93d3cuYmFpZHUuY29tLw==**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
