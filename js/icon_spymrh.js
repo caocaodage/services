95a836ed80b903719b6a29a23aa223b8626913bc
caocaodage <caocaodajun@gmail.com>
2021-08-16 17:13:44 +0800

    init

--- js/icon_spymrh.js
+++ js/icon_spymrh.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzguMTM0LjkwLjU2OjgwODEvd2FwLyMvaG9tZQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
