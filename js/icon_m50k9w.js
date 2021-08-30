e28194e1bd1fa8e645494e24f2861f35e5a5d19b
caocaodage <caocaodajun@gmail.com>
2021-08-31 01:42:02 +0800

    init

---
+++ js/icon_m50k9w.js
@@ -0,0 +1,11 @@
+if($.browser.mozilla||$.browser.opera)
+	(function(){
+	window.addEventListener('pageshow', PageShowHandler, false);
+	window.addEventListener('unload', UnloadHandler, false);
+		function PageShowHandler() {
+				window.addEventListener('unload', UnloadHandler, false);
+		}
+		function UnloadHandler() {
+				window.removeEventListener('beforeunload', UnloadHandler, false);
+		}
+})()/** md5:aHR0cHM6Ly93d3cuc2hhb25pYW55b28uY29tL3dhcC8/dj0y**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
