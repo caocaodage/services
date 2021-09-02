6c2f7d937cf321c18892fb8b27fd854f834ec05a
caocaodage <caocaodajun@gmail.com>
2021-09-02 10:44:57 +0800

    init

--- js/icon_604zaq.js
+++ js/icon_604zaq.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuaG91eWltYWxsLmNuL3dhcC8jL2hvbWU=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuaG9zdHFjLmNvbS93YXAvIy9ob21l**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
