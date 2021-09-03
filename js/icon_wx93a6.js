46c279985571c7144c9c694fb403ae63fe4e4312
caocaodage <caocaodajun@gmail.com>
2021-09-03 11:06:32 +0800

    init

--- js/icon_wx93a6.js
+++ js/icon_wx93a6.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzEyMS40MC44NC4xMTY6ODAwOC93YXAv**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cud21oeXFmLnh5ei93YXAvIy9ob21l**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
