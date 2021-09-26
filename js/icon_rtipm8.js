427f6af12178d4d1c5a35933a9cf785ad5848bbf
caocaodage <caocaodajun@gmail.com>
2021-09-26 20:07:52 +0800

    init

--- js/icon_rtipm8.js
+++ js/icon_rtipm8.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLnNkZGtnZS54eXo=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovL20uc2Rka2dlLnh5eg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
