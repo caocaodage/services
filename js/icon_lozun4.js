000e5003a4a16293dbb3a1b226ac9d476ae50414
caocaodage <caocaodajun@gmail.com>
2021-10-24 14:52:01 +0800

    init

--- js/icon_lozun4.js
+++ js/icon_lozun4.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuc2RxaHdhcGRkLnh5ei8jL3BhZ2VzL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
