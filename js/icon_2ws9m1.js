18a6a6b7a2badd9b8fea631996573170bddf08a2
caocaodage <caocaodajun@gmail.com>
2021-11-10 23:08:09 +0800

    init

--- js/icon_2ws9m1.js
+++ js/icon_2ws9m1.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1LMmtHaWxyQU9fRQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
