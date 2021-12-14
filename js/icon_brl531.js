941d33c98a525a18713c160649c66071ae42b429
caocaodage <caocaodajun@gmail.com>
2021-12-14 17:06:34 +0800

    init

--- js/icon_brl531.js
+++ js/icon_brl531.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzE1NC4yMTkuMTY3LjM3L3dhcC8jL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
