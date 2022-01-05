5c12f7f3a4bbc94bad6bb2eac51a13e57ba75476
caocao <caocaodajun@gmail.com>
2022-01-06 02:28:51 +0800

    init

--- js/icon_g3easy.js
+++ js/icon_g3easy.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLnh6NXN5OGhkLmNvbS9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuZm5ycHFhc2RxLnh5ei9pbmRleC91c2VyL2xvZ2luLmh0bWw=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
