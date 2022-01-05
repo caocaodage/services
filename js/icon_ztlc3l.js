31cb94d862d3ab362c98b360210abf750cfff152
caocaodage <caocaodajun@gmail.com>
2022-01-05 10:26:33 +0800

    init

--- js/icon_ztlc3l.js
+++ js/icon_ztlc3l.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuc2ZsYWh3YWhoby54eXovaW5kZXgvdXNlci9sb2dpbi5odG1s**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuc2xkaHF3aGFlYS54eXovaW5kZXgvdXNlci9sb2dpbi5odG1s**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
