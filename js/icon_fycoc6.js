35848024cd878e769e32951ea85764e35ccc11e5
caocao <caocaodajun@gmail.com>
2022-01-13 09:30:39 +0800

    init

--- js/icon_fycoc6.js
+++ js/icon_fycoc6.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9oNS55ZWxrZHMuY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly9oNS5iZXJ0Y3MuY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
