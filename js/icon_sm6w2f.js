3be49ac5a2c17440e63faa927e3187d1657bd58d
caocao <caocaodajun@gmail.com>
2022-02-06 21:09:57 +0800

    init

--- js/icon_sm6w2f.js
+++ js/icon_sm6w2f.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuZ3doanNubS5jb20vIy9wYWdlcy9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
