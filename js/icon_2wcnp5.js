85bc8861542af144e77f7148610f0e6af47ac616
caocao <caocaodajun@gmail.com>
2022-02-23 22:44:46 +0800

    init

--- js/icon_2wcnp5.js
+++ js/icon_2wcnp5.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJzZGhic2puYy54eXo=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly9tLmJzZGhic2puYy54eXovbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
