ac6eccd9881bd1b29cba0aec50b7da1583561058
caocao <caocaodajun@gmail.com>
2022-03-01 10:43:16 +0800

    init

--- js/icon_p0gr22.js
+++ js/icon_p0gr22.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZWFiY3NxLnh5ei93YXA=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuZWFiY3NxLnh5ei93YXAvIy9sb2dpbjI=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
