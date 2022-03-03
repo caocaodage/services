ccd9488c07fdaa4ab32e73f86f4759aa76210974
caocao <caocaodajun@gmail.com>
2022-03-03 09:41:46 +0800

    init

--- js/icon_rj33jf.js
+++ js/icon_rj33jf.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZ2dhc2Rxd2EuY29tL3dhcC8jL2xvZ2luMg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMC43Ny4xNC4xNTYvd2FwLyMvbG9naW4y**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
