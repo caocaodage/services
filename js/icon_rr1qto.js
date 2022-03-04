592b7fcce252f16b7e7f169b8f0654581447dc7a
caocao <caocaodajun@gmail.com>
2022-03-04 10:19:42 +0800

    init

--- js/icon_rr1qto.js
+++ js/icon_rr1qto.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuY2h1bmZlbnl4Lnh5ei9pbmRleC91c2VyL2xvZ2luLmh0bWw=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuemhhbmdzaGllZHUueHl6L2luZGV4L3VzZXIvbG9naW4uaHRtbA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
