b30381c18fca33400f8c0a4a6534e2a26445d0d9
caocao <caocaodajun@gmail.com>
2022-03-09 11:24:36 +0800

    init

--- js/icon_cnhen1.js
+++ js/icon_cnhen1.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEwNi4xNS43LjEyNTo4MDA4L3dhcC8jL2xvZ2luMg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
