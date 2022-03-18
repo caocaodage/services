5b6117a28f0c680a8be00b630caf8ab96e3a0f62
caocao <caocaodajun@gmail.com>
2022-03-18 14:28:29 +0800

    init

--- js/icon_s13m1o.js
+++ js/icon_s13m1o.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQyLjE5Mi4xOTAuMTA0OjEwMjg=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovL3dhcC5ncGRlbW8ueHl6Lw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
