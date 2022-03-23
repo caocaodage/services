de49ca53e8c50770dc9f7fadf7fd4dff231b7819
caocao <caocaodajun@gmail.com>
2022-03-23 14:23:08 +0800

    init

--- js/icon_k3flxk.js
+++ js/icon_k3flxk.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjExMi4xNjEuNDc6ODAwOC93YXAvIy9sb2dpbjI=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuZnNnYXdxZC5jb20vd2FwLyMvbG9naW4y**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
