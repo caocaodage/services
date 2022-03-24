ee8acf96206b337692533637fcb7a64658118165
caocao <caocaodajun@gmail.com>
2022-03-24 12:53:10 +0800

    init

--- js/icon_bunytg.js
+++ js/icon_bunytg.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuY29hc3RsaW5lZS5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:5Yig6Zmk**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
