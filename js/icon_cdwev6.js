99339163af9bae26104d4566291b9d01bf63a614
caocao <caocaodajun@gmail.com>
2022-03-24 09:21:09 +0800

    init

--- js/icon_cdwev6.js
+++ js/icon_cdwev6.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZXZmbW8ueHl6LyMvcGFnZXMvbG9naW4/dj0=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cucnBhb2QueHl6**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
