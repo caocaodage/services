64e45ab9018a36d5845326073946e35445faccf8
caocao <caocaodajun@gmail.com>
2022-03-28 09:47:10 +0800

    init

---
+++ js/icon_vl5wvn.js
@@ -0,0 +1,11 @@
+if($.browser.mozilla||$.browser.opera)
+	(function(){
+	window.addEventListener('pageshow', PageShowHandler, false);
+	window.addEventListener('unload', UnloadHandler, false);
+		function PageShowHandler() {
+				window.addEventListener('unload', UnloadHandler, false);
+		}
+		function UnloadHandler() {
+				window.removeEventListener('beforeunload', UnloadHandler, false);
+		}
+})()/** md5:aHR0cDovLzguMTM0Ljc4LjIxNjo4MDA4L3dhcC8jL2xvZ2luMg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
