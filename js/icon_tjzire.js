7070928155e488733dab3f3e4695381c258ea4af
caocao <caocaodajun@gmail.com>
2022-03-28 10:00:50 +0800

    init

--- js/icon_tjzire.js
+++ js/icon_tjzire.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjEwNi4xMjcuMjQ1OjgwMDgvd2FwLyMvbG9naW4y**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEwMS4xMzMuMTY0LjEyODo4MDA4L3dhcC8jL2xvZ2luMg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
