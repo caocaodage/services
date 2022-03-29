0a60cc8caa59ab76554e218c803fbc13bbfcc302
caocao <caocaodajun@gmail.com>
2022-03-29 16:16:18 +0800

    init

--- js/icon_rtipm8.js
+++ js/icon_rtipm8.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovL20uc2Rka2dlLnh5eg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:5Yig6Zmk**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
