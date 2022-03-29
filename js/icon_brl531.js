5f61cee344d597f10dbdf853687b698b10a26740
caocao <caocaodajun@gmail.com>
2022-03-29 16:15:00 +0800

    init

--- js/icon_brl531.js
+++ js/icon_brl531.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzEyMS40MS4yOC45Mjo4MDA4L3dhcC8jL2xvZ2luMg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:5Yig6Zmk**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
