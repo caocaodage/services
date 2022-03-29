84873432c45f006ecfaf2ed7c7ade2f00e6971fd
caocao <caocaodajun@gmail.com>
2022-03-29 16:13:57 +0800

    init

--- js/icon_ox7yfs.js
+++ js/icon_ox7yfs.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tdi50YWlndW1pbi5jb20vbW9iaWxlL2xvZ2luLmh0bWw=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:5Yig6Zmk**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
