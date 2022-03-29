96647d7968295df2728a0de45ac48862ddf6be43
caocao <caocaodajun@gmail.com>
2022-03-29 16:11:17 +0800

    init

--- js/icon_x4mchj.js
+++ js/icon_x4mchj.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly94YXNsd2MuY29tL2hvbWUvbG9naW4vaW5kZXguaHRtbA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:5Yig6Zmk**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
