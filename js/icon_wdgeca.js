abc032fe0acacf9532ae0e80a298d5cb43ae5ff1
caocao <caocaodajun@gmail.com>
2022-04-04 14:14:50 +0800

    init

--- js/icon_wdgeca.js
+++ js/icon_wdgeca.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cueW5zZXp4LmNvbS93YXA=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cueW5zZXp4LmNvbS93YXAvIy9sb2dpbjI=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
