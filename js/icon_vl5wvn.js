c78e51577f8227d3aecaf36c5cde04969b80be73
caocao <caocaodajun@gmail.com>
2022-04-07 10:12:12 +0800

    init

--- js/icon_vl5wvn.js
+++ js/icon_vl5wvn.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzguMTM0LjcyLjM4OjgwMDgvd2FwLyMvbG9naW4y**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzExMi43NC40NS4yNjo4MDA4L3dhcC8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
