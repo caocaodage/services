11af5f53002e0e4226c4ae0d6deb9c0bd7e8867f
caocao <caocaodajun@gmail.com>
2022-04-08 10:22:07 +0800

    init

--- js/icon_z8je0n.js
+++ js/icon_z8je0n.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuYXNscXAueHl6L3dhcC8jL2xvZ2luMj9fdj1yYW5kb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
