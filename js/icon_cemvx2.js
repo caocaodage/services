8abdeccfbc485cc644980de4ac76e6376e3c2153
caocao <caocaodajun@gmail.com>
2022-04-13 15:38:50 +0800

    init

--- js/icon_cemvx2.js
+++ js/icon_cemvx2.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cubG9zaHIueHl6L3dhcC8jL2xvZ2luMj9fdj1yYW5kb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMC43Ni4xOTcuMTU6ODAwOC93YXAvIy9ob21l**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
