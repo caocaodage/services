a918007c1cfadda369e3cad2d6e5f2f7b986b35b
caocao <caocaodajun@gmail.com>
2022-04-19 10:04:39 +0800

    init

--- js/icon_hnuy87.js
+++ js/icon_hnuy87.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cucWlwc3hjLmNvbS93YXAvIy9sb2dpbjI/X3Y9cmFuZG9t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cucWlwc3hjLmNvbS93YXAvIy9sb2dpbjM/X3Y9cmFuZG9t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
