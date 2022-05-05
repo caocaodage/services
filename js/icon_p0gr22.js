bfb00fcc9dbbbeab4b3c00410cbb17cb4fb423ca
caocao <caocaodajun@gmail.com>
2022-05-05 10:21:43 +0800

    init

--- js/icon_p0gr22.js
+++ js/icon_p0gr22.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzEyMC43Ny41Ny4xMzE6ODAwOC93YXAvIy9sb2dpbjI=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cudndlYXdxZS5jb20vd2Fw**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
