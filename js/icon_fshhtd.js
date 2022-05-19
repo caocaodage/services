1dc278e1f6cd241fcdd52bad8eb89e0528dbb057
caocao <caocaodajun@gmail.com>
2022-05-19 17:08:08 +0800

    init

--- js/icon_fshhtd.js
+++ js/icon_fshhtd.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZGxvaXIueHl6L3dhcC8jL2xvZ2luMw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzguMTM0Ljg0LjQ2OjgwMDgvd2FwLyMvaG9tZQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
