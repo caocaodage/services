e000c3f2a86037a4cebb07efe779fc9c73908f31
caocao <caocaodajun@gmail.com>
2022-05-27 09:53:26 +0800

    init

--- js/icon_1utu2s.js
+++ js/icon_1utu2s.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzM5LjEwOC4xODIuMTU6ODAwOC93YXAvIy9sb2dpbjI/**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cudXRkY2phLnh5ei93YXA=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
