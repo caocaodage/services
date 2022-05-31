381f5b68bd598506f807c15cb17982f5733d51d2
caocao <caocaodajun@gmail.com>
2022-05-31 22:43:15 +0800

    init

--- js/icon_p56wil.js
+++ js/icon_p56wil.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovL3d3dy54ZmRwcmouY29tLyMvcGFnZXMvbWluZS91c2VyaW5mbw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuc2d2d3B3LmNvbS8jL3BhZ2VzL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
