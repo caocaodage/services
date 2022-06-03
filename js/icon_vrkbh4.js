fb3bddfdc10d76a413d8f3fde6a8ed7e74ed5ee3
caocao <caocaodajun@gmail.com>
2022-06-03 20:33:10 +0800

    init

--- js/icon_vrkbh4.js
+++ js/icon_vrkbh4.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cubGFsZ2cuY29tPz0y**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cubGFsZ2cuY29tLz9xdWRhbz0xMzFjZmJiZC1mMDg2LTNjYTEtNzg4MC00MWQzNGYzZjg2MGI=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
