439613c2120f146bd4c9a41a4f05112ee2101e3b
caocao <caocaodajun@gmail.com>
2022-06-03 20:32:32 +0800

    init

--- js/icon_fhlhlk.js
+++ js/icon_fhlhlk.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cubGFsZ2cuY29tPz0x**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cubGFsZ2cuY29tLz9xdWRhbz00YTRkMjNmMi0wOWY1LTc1OTAtYmQ0YS0wYTVjMjc5Zjg5ODM=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
