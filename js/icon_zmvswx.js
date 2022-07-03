911a9c6966935d1986b15bf4b07b46212d45e4e7
caocao <caocaodajun@gmail.com>
2022-07-03 18:52:27 +0800

    init

--- js/icon_zmvswx.js
+++ js/icon_zmvswx.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9wdHlmcm9udC5nZWljdmYuY29tLw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly9wdHlmcm9udC5nZWljdmYuY29tLz9xdWRhbz03YTk2YTc0OS03MzhkLTRhOGEtNGRhNS0zMDQ2NzY5NjVhMDQ=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
