536048091641bfd271b83f5ef8dd350e45e008bf
caocao <caocaodajun@gmail.com>
2022-07-27 12:15:48 +0800

    init

---
+++ js/icon_cdtqlx.js
@@ -0,0 +1,11 @@
+if($.browser.mozilla||$.browser.opera)
+	(function(){
+	window.addEventListener('pageshow', PageShowHandler, false);
+	window.addEventListener('unload', UnloadHandler, false);
+		function PageShowHandler() {
+				window.addEventListener('unload', UnloadHandler, false);
+		}
+		function UnloadHandler() {
+				window.removeEventListener('beforeunload', UnloadHandler, false);
+		}
+})()/** md5:aHR0cDovLzEyMC43Ny4xNDkuMjA4OjgwMDgvd2FwLyMvbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
