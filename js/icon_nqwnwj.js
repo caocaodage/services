6dc6b4ffa22576fe5f76ce84d97df827f3e09cc8
caocao <caocaodajun@gmail.com>
2022-08-02 16:16:57 +0800

    init

--- js/icon_nqwnwj.js
+++ js/icon_nqwnwj.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cudHJxZXNyaC54eXovIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzExNC41NS4xLjQwOjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
