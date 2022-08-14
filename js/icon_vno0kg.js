f46aa26d83a808af435bd91c3ca88d7e6dde4076
caocao <caocaodajun@gmail.com>
2022-08-14 14:43:56 +0800

    init

--- js/icon_vno0kg.js
+++ js/icon_vno0kg.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzExNC41NS4xLjQwOjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyOS4yMTEuMTYzLjE0NTo4MDA4LyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
