7f762ff2c4add3bdb64026644cea9a6bc51ada41
caocao <caocaodajun@gmail.com>
2022-08-21 12:25:52 +0800

    init

--- js/icon_tldo4r.js
+++ js/icon_tldo4r.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuYXJxZnpjZGMuY29tLyMvcGFnZXMvbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuZ3NqZnp4Yy5jb20vIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
