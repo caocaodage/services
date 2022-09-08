cdca1e7d1e1bfb058cd1ab796a3cdf145836bb3e
caocao <caocaodajun@gmail.com>
2022-09-08 15:43:41 +0800

    init

--- js/icon_cipnc6.js
+++ js/icon_cipnc6.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tYWljby45N3p4LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuc2RmaGtrLmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
