22540a301bc5963246702efb7d84d98e0c0f257c
caocao <caocaodajun@gmail.com>
2022-09-09 09:50:45 +0800

    init

--- js/icon_ldn94q.js
+++ js/icon_ldn94q.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjEwOC4xMy4zMzo4MDA4**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzM5LjEwOC4xNTYuMTk5OjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
