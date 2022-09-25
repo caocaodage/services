4744a57c05909651cb199c68ea5f35ce5659ce08
caocao <caocaodajun@gmail.com>
2022-09-25 22:07:21 +0800

    init

--- js/icon_ahnzmu.js
+++ js/icon_ahnzmu.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuZ3FieGl3Zy5jb20vaW5kZXgvdXNlci9sb2dpbi5odG1s**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
