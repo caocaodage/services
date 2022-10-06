4e5ab5abef550bf2946eaa1049fba5b5d75c2dc4
caocao <caocaodajun@gmail.com>
2022-10-06 19:35:32 +0800

    init

--- js/icon_7mvh6w.js
+++ js/icon_7mvh6w.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cua3BheHh4eW4uY29tLyMv**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuem5iY3NuYW13cS5jb20vIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
