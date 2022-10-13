efc2420f9a20315c5c596cba58d9028514c5e410
caocao <caocaodajun@gmail.com>
2022-10-13 19:07:21 +0800

    init

--- js/icon_oh9spi.js
+++ js/icon_oh9spi.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuc3dic2Z6enFnLmNvbS8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cudnJuYnNuc3pjdy5jb20vIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
