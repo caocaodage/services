8ebd4bff9cddd0164cb0a45af130018f2e4a351a
caocao <caocaodajun@gmail.com>
2022-10-13 18:12:32 +0800

    init

--- js/icon_zzabtd.js
+++ js/icon_zzabtd.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cud25zc29rZC54eXovIy9wYWdlcy9sb2dpbj92PTI=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cudGpiemNnZy5jb20vIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
