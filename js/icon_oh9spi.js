da70248508f29fc31be313f421668789423d7e28
caocao <caocaodajun@gmail.com>
2022-10-21 20:13:56 +0800

    init

--- js/icon_oh9spi.js
+++ js/icon_oh9spi.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuam52ZW5xbmRhLmNvbS8jLw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjU3LjE0My42Mzo4MDA4**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
