c1ad496e9e7ac84fddc1ca782b5a5806d9309507
caocao <caocaodajun@gmail.com>
2022-10-21 15:24:48 +0800

    init

--- js/icon_cpi8kk.js
+++ js/icon_cpi8kk.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cubmNuZGFqbmFzZC5jb20vIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cudG5ubmthc25kcS5jb20vIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
