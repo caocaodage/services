c28a63c6a5a1d1a2a590c8797ed3c0e8fad41607
caocao <caocaodajun@gmail.com>
2022-10-23 21:32:56 +0800

    init

--- js/icon_s6lzkf.js
+++ js/icon_s6lzkf.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly91cy5oYWl0YW8uY29tLw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cub3Rhb3Rhby5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
