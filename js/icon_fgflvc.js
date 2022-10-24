95001167e98969ccd5bcb58073ef7761c7cc3d61
caocao <caocaodajun@gmail.com>
2022-10-24 10:34:29 +0800

    init

--- js/icon_fgflvc.js
+++ js/icon_fgflvc.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuY25ha3NhbGFxcWUuY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMC43Ny44Mi4yODo4MDA4LyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
