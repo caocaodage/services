cd285e7607ba6573f68ed5745abd373f178d6882
caocao <caocaodajun@gmail.com>
2022-10-28 15:45:12 +0800

    init

--- js/icon_3v3uft.js
+++ js/icon_3v3uft.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cubGtocmlqLmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuaGpya2tmbWV3bnEuY29tLyMv**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
