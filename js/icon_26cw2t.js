3416cea3f34fcef406c7d637bc44951488db14d1
caocao <caocaodajun@gmail.com>
2022-10-30 01:59:23 +0800

    init

--- js/icon_26cw2t.js
+++ js/icon_26cw2t.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzM5LjEwOC4yMTIuMjIxOjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
