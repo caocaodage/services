dc3494b23b9eeb7c446c51d4f1c06e79644fed81
caocaodage <caocaodajun@gmail.com>
2022-11-02 15:39:00 +0800

    init

--- js/icon_eudwmd.js
+++ js/icon_eudwmd.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZHNkbnNiZmVtcXNhZC5jb20vIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuZHNkbnNiZmVtcXNhZC5jb20vIy8/dj0x**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
