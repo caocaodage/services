93229e7e9bb35a4a571ae0b760a1e970e026e3fe
caocaodage <caocaodajun@gmail.com>
2022-11-05 13:27:16 +0800

    init

--- js/icon_dwexhg.js
+++ js/icon_dwexhg.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cudm5tbmFzbmRhai5jb20vIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuZ25yaGhxandla2ouY29tLyMv**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
