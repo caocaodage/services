65700b4556f69f412ec80ee8d23755c4fbf2d8f3
caocaodage <caocaodajun@gmail.com>
2022-11-08 09:19:50 +0800

    init

--- js/icon_8sr2pm.js
+++ js/icon_8sr2pm.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzEyMC43Ny4xNzguMTQ2OjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMC43OS4yMC4yNDQ6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
