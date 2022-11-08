f168eaf1bf53f0f93d1c7c7f16011ce01ef22736
caocaodage <caocaodajun@gmail.com>
2022-11-08 13:24:39 +0800

    init

--- js/icon_zzabtd.js
+++ js/icon_zzabtd.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZ3JoaHFpd2pza3FlLmNvbS8jLw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cudmJoYmZuYW5rcWVxLmNvbS8jLw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
