e910c9f5db022a604bb2dfb91c9b5ce12885f16a
caocaodage <caocaodajun@gmail.com>
2022-11-09 22:43:19 +0800

    init

--- js/icon_bowhpx.js
+++ js/icon_bowhpx.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuYWx5Y2FwcGEuY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuYWx5Y2NjYy5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
