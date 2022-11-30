6c4bbc6e5e0a86d73d219adf581d02837dd6fcef
caocaodage <caocaodajun@gmail.com>
2022-11-30 09:21:59 +0800

    init

--- js/icon_1s7p9g.js
+++ js/icon_1s7p9g.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzM5LjEwOC43NC4xNTg6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuc2lmaHZleXVxd2llLmNvbS8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
