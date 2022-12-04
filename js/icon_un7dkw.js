a4283ed979ae51336504cee8040ebf34574b997d
caocaodage <caocaodajun@gmail.com>
2022-12-04 14:45:04 +0800

    init

--- js/icon_un7dkw.js
+++ js/icon_un7dkw.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cubmNuZW5tbWFzLnh5eg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cudGl3b2lyb2l3b3EuY29tLyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
