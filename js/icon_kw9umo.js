c439ce0c0b9e1c6fb150f2cf53dcaf7a117e6f61
caocaodage <caocaodajun@gmail.com>
2022-12-06 22:06:46 +0800

    init

--- js/icon_kw9umo.js
+++ js/icon_kw9umo.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly90Z2Zyb250ZGV2LnNnY3Z3dy54eXov**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
