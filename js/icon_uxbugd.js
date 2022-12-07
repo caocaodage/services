b0e80369a2767f892d8f98fa2987844a58837aee
caocaodage <caocaodajun@gmail.com>
2022-12-07 09:22:16 +0800

    init

--- js/icon_uxbugd.js
+++ js/icon_uxbugd.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZ3Jtd3FuZWJiYXNtZS5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEzOS4xOTYuMTk0LjIyNDo4MDA4LyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
