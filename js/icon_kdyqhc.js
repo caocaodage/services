087244039f845668230550682d0132c9d6baf7dc
caocaodage <caocaodajun@gmail.com>
2022-12-15 16:12:25 +0800

    init

--- js/icon_kdyqhc.js
+++ js/icon_kdyqhc.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjEyMi4yMS4xMzM6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjEyMi4xNy4yMDU6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
