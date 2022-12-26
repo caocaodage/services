c1633236082625ca48446747c81a7020ffc4d10f
caocaodage <caocaodajun@gmail.com>
2022-12-26 16:47:12 +0800

    init

--- js/icon_83ir2y.js
+++ js/icon_83ir2y.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzE5Mi4xNjguLjkuNTQ6MzEwMC9pbmRleC5odG1sIy9wYWdlcy9ob21lL3Rlc3Q=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzE5Mi4xNjguOS41NDozMTAwL2luZGV4Lmh0bWwjL3BhZ2VzL2hvbWUvdGVzdA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
