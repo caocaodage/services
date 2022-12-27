fb89df0449f354e493fd7c3d704a22f1d56b064b
caocaodage <caocaodajun@gmail.com>
2022-12-27 18:06:56 +0800

    init

--- js/icon_ld1cic.js
+++ js/icon_ld1cic.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzEyMC43OC4xNTQuOTg6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMC43OC4xNTQuOTg6ODAwOC8jL2hvbWUvaW5kZXg=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
