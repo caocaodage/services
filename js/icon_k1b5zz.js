a36bd6963f601e08103ed0034596f06c44251b08
caocaodage <caocaodajun@gmail.com>
2023-01-03 17:20:27 +0800

    init

--- js/icon_k1b5zz.js
+++ js/icon_k1b5zz.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjEyMi4yMS4xNTc6ODAwOC8jL2hvbWUvaW5kZXg=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjkyLjEyNy4xMTo4MDA4LyMvaG9tZS9pbmRleA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
