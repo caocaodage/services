db184448812f6a07f70af263beef91499dd1fcd6
caocaodage <caocaodajun@gmail.com>
2023-01-04 14:58:05 +0800

    init

--- js/icon_ba8z84.js
+++ js/icon_ba8z84.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cubWZhbm4xbDFmLmNvbS8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly90ZXN0**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
