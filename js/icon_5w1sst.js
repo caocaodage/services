60898728d7a65b6d05b7a043951b4d70675ee792
caocaodage <caocaodajun@gmail.com>
2023-01-11 10:16:48 +0800

    init

--- js/icon_5w1sst.js
+++ js/icon_5w1sst.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZ2p0andlcmtsZG1zLmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzMuMS44Ny4yMjI6ODAwOC8jLw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
