70d6d24461655d5ddef496b90fde39a6f03452ce
caocaodage <caocaodajun@gmail.com>
2023-01-13 10:39:57 +0800

    init

--- js/icon_vw5ct8.js
+++ js/icon_vw5ct8.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuMW5qd2puYWIxMW52LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjEwNC4xNjMuMjk6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
