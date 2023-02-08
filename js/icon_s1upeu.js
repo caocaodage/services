adb1af909c42e4395c09b318831d26ee3c8d0599
caocaodage <caocaodajun@gmail.com>
2023-02-08 16:36:27 +0800

    init

--- js/icon_s1upeu.js
+++ js/icon_s1upeu.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuYnRnam5qYW5lamguY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuYnRnam5qYW5lamguY29tP3Y9Mg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
