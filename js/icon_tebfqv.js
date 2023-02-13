277666b7634d6599b5407f4b323a2787b5641acc
caocaodage <caocaodajun@gmail.com>
2023-02-13 11:35:38 +0800

    init

--- js/icon_tebfqv.js
+++ js/icon_tebfqv.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuc2pmaGpzZGYueHl6L2g1LyMv**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuc2pmaGpzZGYueHl6L2g1LyM=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
