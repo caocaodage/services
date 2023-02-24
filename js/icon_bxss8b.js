cff1a72161a0e1d0f67d2bfd64aa431376f74eb1
caocaodage <caocaodajun@gmail.com>
2023-02-24 09:03:35 +0800

    init

--- js/icon_bxss8b.js
+++ js/icon_bxss8b.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZ3J1ZXJ5Z2hxd3EuY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzE4LjEzOS4yMTAuMjQzOjgwMDgvIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
