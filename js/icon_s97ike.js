0ec0f64add2bc8e4fd3742dfd98e61146c2f5b71
caocaodage <caocaodajun@gmail.com>
2023-02-24 12:58:52 +0800

    init

--- js/icon_s97ike.js
+++ js/icon_s97ike.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuc2puY252d2plaHUuY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzE4LjE0MC40LjE0MDo4MDA4LyMv**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
