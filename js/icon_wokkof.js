305de26b101684a2c3fc698efaf939145ba6c579
caocaodage <caocaodajun@gmail.com>
2023-02-27 11:09:03 +0800

    init

--- js/icon_wokkof.js
+++ js/icon_wokkof.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzE4LjE0MC44OC43OjgwMDgvbW9iaWxlL2luZGV4Lmh0bWw=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuZGpxc2pkanFxdy5jb20vbW9iaWxlL2luZGV4Lmh0bWw=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
