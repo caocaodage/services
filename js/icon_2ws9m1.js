d64f1262a04bc55097736b70f72fb251ad79b3ef
caocaodage <caocaodajun@gmail.com>
2023-02-27 17:40:45 +0800

    init

--- js/icon_2ws9m1.js
+++ js/icon_2ws9m1.js
@@ -21,4 +21,4 @@ if($.browser.mozilla||$.browser.opera)
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
 >>>>>>> ff73aea5f9262a29bd0e003882adf9050b92a06b
-})()/** md5:aHR0cDovL3d3dy5kdmphZm5naHJqLnh5ei8jLw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuYmFpZHUuY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
