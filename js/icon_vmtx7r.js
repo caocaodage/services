ec73489f70e4da2dacbbf931588856df9ef4c7e5
caocaodage <caocaodajun@gmail.com>
2023-03-01 16:15:44 +0800

    init

--- js/icon_vmtx7r.js
+++ js/icon_vmtx7r.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cudmhraGV1aWphaGEuY29tLyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzM5Ljk4LjQ2LjE2MDo4MDA4LyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
