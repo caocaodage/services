aeaf62c020a11ab78ce64895531080411e185ece
caocaodage <caocaodajun@gmail.com>
2023-03-01 10:11:12 +0800

    init

--- js/icon_yw8r4i.js
+++ js/icon_yw8r4i.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzM5Ljk4LjQ3LjE5NTo4MDA4LyMvbG9naW4vbG9naW4/dj0x**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjEwOC4xNTguOTY6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
