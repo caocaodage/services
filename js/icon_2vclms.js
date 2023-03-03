8c2ec570943940dfd91cde28d81dd02b1e505d87
caocaodage <caocaodajun@gmail.com>
2023-03-03 08:27:35 +0800

    init

--- js/icon_2vclms.js
+++ js/icon_2vclms.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuYnRqbndrZWpybnNtay5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuYm5nampxb2Vqbi5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
