8dda206f3bdbd7932b85a887b1803c7eb466a3c6
caocaodage <caocaodajun@gmail.com>
2023-03-05 20:30:08 +0800

    init

--- js/icon_7bsnv7.js
+++ js/icon_7bsnv7.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzEzLjI1MS4yNTEuMjo4MDA4LyMv**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cudnJocWVpamFoeGEuY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
