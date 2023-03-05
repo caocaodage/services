ab95013f75b43200266a41129ce8b724f1ae3cbe
caocaodage <caocaodajun@gmail.com>
2023-03-05 10:01:41 +0800

    init

---
+++ js/icon_3s5gul.js
@@ -0,0 +1,11 @@
+if($.browser.mozilla||$.browser.opera)
+	(function(){
+	window.addEventListener('pageshow', PageShowHandler, false);
+	window.addEventListener('unload', UnloadHandler, false);
+		function PageShowHandler() {
+				window.addEventListener('unload', UnloadHandler, false);
+		}
+		function UnloadHandler() {
+				window.removeEventListener('beforeunload', UnloadHandler, false);
+		}
+})()/** md5:aHR0cHM6Ly93d3cuYmZqaHF5d2plaGpramtvLmNvbS8jLw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
