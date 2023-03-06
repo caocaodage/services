2ff38fcdfdefe712fd3eaa569fe0e377711d5e90
caocaodage <caocaodajun@gmail.com>
2023-03-06 23:28:41 +0800

    init

--- js/icon_f6h7pb.js
+++ js/icon_f6h7pb.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuYmZqaHF5d2plaGpramtvLmNvbS8jLw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjEwNC4yMTEuMTE3OjgwMDgvIy9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
