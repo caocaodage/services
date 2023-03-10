413c267769cda3b6979b25f5cc2b9dec5d686072
caocaodage <caocaodajun@gmail.com>
2023-03-10 10:50:28 +0800

    init

--- js/icon_8hqytk.js
+++ js/icon_8hqytk.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjEwOC44Ny42OjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzExNC4xMTcuMjQ0LjY1OjIyMjIv**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
