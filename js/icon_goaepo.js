8e56dabdc05ffefd663b96edd648153b6fbaae1a
caocaodage <caocaodajun@gmail.com>
2023-03-11 14:04:53 +0800

    init

--- js/icon_goaepo.js
+++ js/icon_goaepo.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzEuMTMuMjQ2LjgxOjgwMDgvIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzExOS4yMy4yNTQuMTkzOjgwMDgv**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
