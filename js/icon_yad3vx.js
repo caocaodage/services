116a6c9a14ee077dcc1553231c4dde6ae0745b30
caocaodage <caocaodajun@gmail.com>
2023-03-17 09:24:39 +0800

    init

--- js/icon_yad3vx.js
+++ js/icon_yad3vx.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cueGttYXd1ZWpxdWphamQuY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovL3d3dy5na2pycnF3dy54eXovIy9wYWdlcy9sb2dpbj92PTI=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
