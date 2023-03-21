62d39df9246128d8c701383dfe3c6854101348e7
caocaodage <caocaodajun@gmail.com>
2023-03-21 19:29:30 +0800

    init

--- js/icon_x5ycnv.js
+++ js/icon_x5ycnv.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cudHJzYy50b3AvIy9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly94Zy5iYWFnZ28uY29tLyMvbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
