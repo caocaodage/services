8d156921b49ab5a9c80de5d32364926a4757a5f2
daguo <daguo@gmail.com>
2023-03-22 22:13:41 +0800

    init

--- js/icon_2ws9m1.js
+++ js/icon_2ws9m1.js
@@ -21,4 +21,4 @@ if($.browser.mozilla||$.browser.opera)
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
 >>>>>>> ff73aea5f9262a29bd0e003882adf9050b92a06b
-})()/** md5:aHR0cDovL3d3dy5hbW53anFla2lrYWh3LmNvbS8/dj01**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovL3d3dy5hbW53anFla2lrYWh3LmNvbS8/dj02**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
