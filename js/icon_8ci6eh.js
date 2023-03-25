8d1d6f7260ffa1f584b0959b432d7b19d8268e83
daguo <daguo@gmail.com>
2023-03-25 13:44:38 +0800

    init

--- js/icon_8ci6eh.js
+++ js/icon_8ci6eh.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cueWFiY2JodGEuY29tLyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyOS4yMTEuMjI0LjIzNjo4MDA4LyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
