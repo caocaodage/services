74a9aeba1540e4620a81fd87b70f5633fb60c117
daguo <daguo@gmail.com>
2023-03-25 14:53:24 +0800

    init

--- js/icon_3cbumk.js
+++ js/icon_3cbumk.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cudml1YWViYS5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzUyLjIyMC40OC4xMzY6ODAwOC8jLw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
