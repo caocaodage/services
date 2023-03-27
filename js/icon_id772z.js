c31de5b9e78b13c791eb64676aef1472a1d0f2c6
daguo <daguo@gmail.com>
2023-03-27 22:37:47 +0800

    init

--- js/icon_id772z.js
+++ js/icon_id772z.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly9hcHAucWV5ZmJ1Yi5jb20vIy9wYWdlcy9sb2dpbi9pbmRleA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
