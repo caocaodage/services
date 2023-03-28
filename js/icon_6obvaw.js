1b1a649387418ffda2325ad1018b946d4c241c76
daguo <daguo@gmail.com>
2023-03-28 11:21:02 +0800

    init

--- js/icon_6obvaw.js
+++ js/icon_6obvaw.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzExNC4xMTcuMTc0LjIzNTo4MDA4LyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMC43OC4yMTQuMTY4OjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
