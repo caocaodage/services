0dcd448262e0d28c8ec6b38d1c9ed4d66fbcdede
daguo <daguo@gmail.com>
2023-03-29 19:16:32 +0800

    init

--- js/icon_2vclms.js
+++ js/icon_2vclms.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuemRoZ3dxaGV3ZS5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cueXhiZGhhZXEuY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
