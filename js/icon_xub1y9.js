eb325a730842f274b311f4d11f9d2f7dc7bca123
daguo <daguo@gmail.com>
2023-03-30 08:50:57 +0800

    init

--- js/icon_xub1y9.js
+++ js/icon_xub1y9.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzM5Ljk4LjE5MS4yMzc6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEzMi4yMzIuODYuMjE3OjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
