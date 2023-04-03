412c19f93bf9f9b68efd639a5c65950cd546fdc3
daguo <daguo@gmail.com>
2023-04-03 09:19:35 +0800

    init

--- js/icon_7dsh50.js
+++ js/icon_7dsh50.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzM5Ljk4LjIwNi4xNDI6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEzOS4xNTUuODUuMjI5OjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
