ed1c710e16e57976b00e22d1e821232e2adbe1c3
daguo <daguo@gmail.com>
2023-04-05 02:06:59 +0800

    init

--- js/icon_71qmh2.js
+++ js/icon_71qmh2.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjEwOS40NC4yMjI6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
