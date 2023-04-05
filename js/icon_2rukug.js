f664081c161678ebbe46f092ea61f6946a42a8d9
daguo <daguo@gmail.com>
2023-04-05 14:58:10 +0800

    init

--- js/icon_2rukug.js
+++ js/icon_2rukug.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuemlham5zaG1pLmNvbS8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuemlham5zaG1pLmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
