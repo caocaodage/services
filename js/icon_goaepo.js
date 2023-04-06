2928c6a61032ae19e5ce46c502512fc98ad15683
daguo <daguo@gmail.com>
2023-04-06 17:15:38 +0800

    init

--- js/icon_goaepo.js
+++ js/icon_goaepo.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzEuMTMuMjQ2LjgxOjgwMDgvIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzM5Ljk4LjE3MC4yMTk6ODAwOC8jLw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
