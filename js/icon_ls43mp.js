fd86f854a2fd241e210e53870af99794ef4e7591
daguo <daguo@gmail.com>
2023-04-09 13:11:39 +0800

    init

--- js/icon_ls43mp.js
+++ js/icon_ls43mp.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzE0OC43MC4xNTcuMjMxOjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzExMi43NC4xODAuMjI2OjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
