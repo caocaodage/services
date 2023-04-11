7c31d6f747a69f384c77764d0bf88b8f81fb7e74
daguo <daguo@gmail.com>
2023-04-11 14:24:44 +0800

    init

--- js/icon_fdagfe.js
+++ js/icon_fdagfe.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzExOC4yNC4xMTAuNzI6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzExOC4yNC4xMTAuNzI6ODAwOC8jL2xvZ2luL2xvZ2luP3Y9Mg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
