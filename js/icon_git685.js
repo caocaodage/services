848fe410ba6172f0e555783f660bb9d1c9bdff98
daguo <daguo@gmail.com>
2023-04-12 19:59:09 +0800

    init

--- js/icon_git685.js
+++ js/icon_git685.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjEwNi4xNDQuMTQwOjgwMDgv**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cueWNmNjg2ODY4LmNvbS8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
