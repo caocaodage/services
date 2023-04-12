388ed2c0de0ec5b5645089b4a09e1d4e573b6a32
daguo <daguo@gmail.com>
2023-04-13 00:00:19 +0800

    init

--- js/icon_8pia3d.js
+++ js/icon_8pia3d.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:MTIwLjc5LjMwLjE4NA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuemhvbmdtZW5nY2hlbnh4aS5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
