17b0a5702e6233e928da2d0728e9410b824cac46
daguo <daguo@gmail.com>
2023-04-12 13:07:00 +0800

    init

--- js/icon_f18pp8.js
+++ js/icon_f18pp8.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cueXVuY2FpZnU4NjkuY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMC43OS44Ni4xNzg6ODAwOC8jLw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
