13047d7085eded0758dc6bdb825ce11a8ec084d3
daguo <daguo@gmail.com>
2023-04-13 15:32:06 +0800

    init

--- js/icon_c4m0z5.js
+++ js/icon_c4m0z5.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuY29hb2FjZC54eXo=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuY29hb2FjZC54eXo/dj0x**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
