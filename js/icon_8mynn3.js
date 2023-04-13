e87cd853b12b6ae57ce8eeb4c48c3958251e87eb
daguo <daguo@gmail.com>
2023-04-13 10:39:54 +0800

    init

--- js/icon_8mynn3.js
+++ js/icon_8mynn3.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()
+})()/** md5:aHR0cDovLzEyMC43OS40Ny4xMDM6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
