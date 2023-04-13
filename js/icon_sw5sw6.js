5de6ff2bd6c7b36002a428bbf9749dccacde5987
daguo <daguo@gmail.com>
2023-04-13 15:33:30 +0800

    init

--- js/icon_sw5sw6.js
+++ js/icon_sw5sw6.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuY25vYWluYy54eXo=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuY25vYWluYy54eXo/dj0x**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
