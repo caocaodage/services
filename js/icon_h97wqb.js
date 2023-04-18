1167de4b7e70409b92533ba08f9550dde5dec5ca
daguo <daguo@gmail.com>
2023-04-18 09:27:24 +0800

    init

--- js/icon_h97wqb.js
+++ js/icon_h97wqb.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzM5Ljk4LjE5OS40NTo4MDA4LyMvbG9naW4vbG9naW4/dj0z**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzM5Ljk4LjQyLjIwOjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
