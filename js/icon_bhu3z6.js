d30dd71dc37075d15dd29ec365f9bfb97343ab98
daguo <daguo@gmail.com>
2023-04-20 09:56:43 +0800

    init

--- js/icon_bhu3z6.js
+++ js/icon_bhu3z6.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjkyLjIzNC4xNjM6ODAwOC8jL2xvZ2luL2xvZ2luLGh0dHBzOi8vd3d3Lmdvb2dsZS5jb20saHR0cHM6Ly9tLmJhaWR1LmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjkyLjIzNC4xNjM6ODAwOC8jL2xvZ2luL2xvZ2luLA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
