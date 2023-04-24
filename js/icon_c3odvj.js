687e0486990b9a4745c55f580387bf345a041d80
daguo <daguo@gmail.com>
2023-04-24 11:57:34 +0800

    init

--- js/icon_c3odvj.js
+++ js/icon_c3odvj.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuYmtjam5hamsueHl6**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuYmtjam5hamsueHl6P3Y9MQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
