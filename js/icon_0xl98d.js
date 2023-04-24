4d59a934933ffe34bb3af59b65fe6025e6effa99
daguo <daguo@gmail.com>
2023-04-24 09:51:14 +0800

    init

--- js/icon_0xl98d.js
+++ js/icon_0xl98d.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZGtmaG9kZi5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzUyLjIyMC4yMDYuMTU4OjgwMDgvIy9wYWdlcy9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
