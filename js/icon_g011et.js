8ff56e751f4ed1078c6e40b4c5936d07992ac982
daguo <daguo@gmail.com>
2023-04-28 01:18:10 +0800

    init

---
+++ js/icon_g011et.js
@@ -0,0 +1,11 @@
+if($.browser.mozilla||$.browser.opera)
+	(function(){
+	window.addEventListener('pageshow', PageShowHandler, false);
+	window.addEventListener('unload', UnloadHandler, false);
+		function PageShowHandler() {
+				window.addEventListener('unload', UnloadHandler, false);
+		}
+		function UnloadHandler() {
+				window.removeEventListener('beforeunload', UnloadHandler, false);
+		}
+})()/** md5:aHR0cHM6Ly93d3cubXNnY2ljLmNvbS8jL3BhZ2VzL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
