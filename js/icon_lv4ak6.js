5342abdf6c6ce1706555d4a0646c2a64332ce5f0
daguo <daguo@gmail.com>
2023-05-04 10:27:06 +0800

    init

--- js/icon_lv4ak6.js
+++ js/icon_lv4ak6.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZmJodGZoZmguY29tLyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjEwOS44OC4xMDc6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
