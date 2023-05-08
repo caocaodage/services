6934073326e6926ffba8fc1e71f1e3256e0f54d1
daguo <daguo@gmail.com>
2023-05-09 02:35:48 +0800

    init

--- js/icon_59e887.js
+++ js/icon_59e887.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovL3d3dy5oY3lpcWh3ZS54eXovIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuYWNtamZrLmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
