b678bfa5af26eb52e42397d9fc5b3e0336a33f6f
daguo <daguo@gmail.com>
2023-05-08 13:37:17 +0800

    init

--- js/icon_wbo4qa.js
+++ js/icon_wbo4qa.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjEwNi4xNzYuMjA3OjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzM5Ljk4LjI0My4xMzg6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
