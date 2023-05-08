15794797e740676429b1d5d70f4238b9c0cf02b2
daguo <daguo@gmail.com>
2023-05-08 09:09:44 +0800

    init

--- js/icon_xltuoz.js
+++ js/icon_xltuoz.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzEyMC43OC4xNDcuMTQwOjgwMDgvIy9sb2dpbi9sb2dpbj92PTE=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cueXhic2d0Y3YuY29tLyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
