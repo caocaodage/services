8208dac04dcf1e57d671fa84babb8757d31c4e9f
daguo <daguo@gmail.com>
2023-05-09 13:50:23 +0800

    init

--- js/icon_bt4vr1.js
+++ js/icon_bt4vr1.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cubmNramFuc2tqLnh5eg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cubmNramFuc2tqLnh5ej92PTE=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
