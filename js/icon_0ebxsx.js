e35d66260be52e11e8d3fff4343cd9c3da8e5211
daguo <daguo@gmail.com>
2023-05-10 22:30:19 +0800

    init

--- js/icon_0ebxsx.js
+++ js/icon_0ebxsx.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjEwOC4zMC45Njo4MDA4LyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
