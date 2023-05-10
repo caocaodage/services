9ede63f6049dcb25b54300ece669eae48bc2d57f
daguo <daguo@gmail.com>
2023-05-11 00:21:58 +0800

    init

--- js/icon_3tlxmx.js
+++ js/icon_3tlxmx.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzEyMi4yNDguMjQ5LjE3OTo4MDA4LyMvP3Y9MQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEzLjIxMi4yMDguMjA6ODAwOC8jLw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
