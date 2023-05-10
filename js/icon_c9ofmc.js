242626c660d66ea553cd88346c3241b74cdd6de6
daguo <daguo@gmail.com>
2023-05-11 00:16:08 +0800

    init

--- js/icon_c9ofmc.js
+++ js/icon_c9ofmc.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzU0LjI1MS4yMzAuMjAzOjgwMDgvIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuenhodmRqcXdpdWVxay5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
