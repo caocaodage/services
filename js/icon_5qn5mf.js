e945754606b3b8578afed0a2c9652119c95db40f
daguo <daguo@gmail.com>
2023-05-15 21:23:00 +0800

    init

--- js/icon_5qn5mf.js
+++ js/icon_5qn5mf.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzExNC41NS42Ni4yNDM6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjkyLjM2Ljk6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
