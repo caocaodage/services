ec8d8e275811fbe8df2d030b7b90b2e70aa6e50f
daguo <daguo@gmail.com>
2023-05-17 14:52:10 +0800

    init

--- js/icon_q4033h.js
+++ js/icon_q4033h.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzExNC41NS4xNDQuMTEzOjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzguMTM0LjM2LjE4MDo4MDA4LyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
