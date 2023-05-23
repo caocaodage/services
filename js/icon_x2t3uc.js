2445fc3fc744e0afba59a75f6b20a4707cc3d1ad
daguo <daguo@gmail.com>
2023-05-23 11:39:09 +0800

    init

--- js/icon_x2t3uc.js
+++ js/icon_x2t3uc.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3Ljk2LjExOC4xMTo4MDA4LyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjEwOC4yMDUuNzM6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
