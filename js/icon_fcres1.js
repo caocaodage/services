85429294d7a2d0a8a3d6ba9b0357ed0513728e23
daguo <daguo@gmail.com>
2023-05-23 12:09:40 +0800

    init

--- js/icon_fcres1.js
+++ js/icon_fcres1.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjEwOS44OS4xNDk6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMS4xOTkuNzcuMTIzOjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
