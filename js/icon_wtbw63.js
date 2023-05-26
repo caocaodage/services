1fda52a41914bfc9acb8d6833799af2f744580e8
daguo <daguo@gmail.com>
2023-05-26 15:25:00 +0800

    init

--- js/icon_wtbw63.js
+++ js/icon_wtbw63.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cueHZocnVpc2hlai54eXo=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuaXZidG5odWhxZXFzLmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
