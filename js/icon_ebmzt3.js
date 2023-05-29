1d8abef4f09a0f1a0d2b584e3a24c21bb5b9fe4b
daguo <daguo@gmail.com>
2023-05-29 13:07:03 +0800

    init

--- js/icon_ebmzt3.js
+++ js/icon_ebmzt3.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cueXhic2d0Y3YuY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3Ljk5LjQxLjEwNTo4MDA4LyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
