3aa152791382ce1429168f94a0ac919946934243
daguo <daguo@gmail.com>
2023-05-31 16:57:17 +0800

    init

--- js/icon_z9jn8u.js
+++ js/icon_z9jn8u.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzExMi43NC40My4xNjA6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cua2Nudmtxd3N3aC5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
