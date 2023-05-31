a07d9f2cd4c693168e52828188724f323e84f48b
daguo <daguo@gmail.com>
2023-05-31 14:44:45 +0800

    init

--- js/icon_db5ogp.js
+++ js/icon_db5ogp.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuYmd1cndxdXdlamFzLmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzUyLjc0LjE2OS43Nzo4MDA4**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
