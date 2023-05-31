bba3d14957abbed8aed929757980d1ff28abab6d
daguo <daguo@gmail.com>
2023-05-31 14:36:44 +0800

    init

--- js/icon_nr0jes.js
+++ js/icon_nr0jes.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZHNuZ2ZoZ2EuY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzE4LjE0My4xNDEuMjI2OjgwMDg=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
