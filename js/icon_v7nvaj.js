25e5bfd73e285b0b6e83df857d8c38deeaa30877
daguo <daguo@gmail.com>
2023-06-01 15:06:25 +0800

    init

--- js/icon_v7nvaj.js
+++ js/icon_v7nvaj.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjEwOC4xNDQuMTYzOjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuZmJodGZoZmguY29tLyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
