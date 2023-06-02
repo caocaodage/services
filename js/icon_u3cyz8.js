94a40c6a0c5317b1f0211f7a440492269ffb818d
daguo <daguo@gmail.com>
2023-06-02 22:03:31 +0800

    init

--- js/icon_u3cyz8.js
+++ js/icon_u3cyz8.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjEwOS43OS4xNDQ6ODAwMg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzguMTM3LjE0LjM0OjgwMDI=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
