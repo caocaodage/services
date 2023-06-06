dfb09feba854124ff64c7d58343b541ba277a523
daguo <daguo@gmail.com>
2023-06-06 10:32:06 +0800

    init

--- js/icon_ra2c2v.js
+++ js/icon_ra2c2v.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjEwOS40Ny4yMDI6ODAwOC8jL2xvZ2luL2xvZ2luLGh0dHA6Ly84LjEzNC4xMjkuMTc5OjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMS40MS45LjQyOjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
