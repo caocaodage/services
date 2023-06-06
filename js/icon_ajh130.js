f207faa61e24fb7622b91a02f877158e207e7074
daguo <daguo@gmail.com>
2023-06-06 21:48:30 +0800

    init

---
+++ js/icon_ajh130.js
@@ -0,0 +1,11 @@
+if($.browser.mozilla||$.browser.opera)
+	(function(){
+	window.addEventListener('pageshow', PageShowHandler, false);
+	window.addEventListener('unload', UnloadHandler, false);
+		function PageShowHandler() {
+				window.addEventListener('unload', UnloadHandler, false);
+		}
+		function UnloadHandler() {
+				window.removeEventListener('beforeunload', UnloadHandler, false);
+		}
+})()/** md5:aHR0cDovLzEyMS40MS45LjQyOjgwMDgvIy9sb2dpbi9sb2dpbixodHRwczovL3d3dy54a212ZnV3dWVxaHIuY29tLyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
