ce502192aa52431daf1a92b267c86c93ee681663
daguo <daguo@gmail.com>
2023-06-09 23:10:34 +0800

    init

--- js/icon_pmwqso.js
+++ js/icon_pmwqso.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovL3d3dy5ia3J1aXF1ZXdoaC5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzguMTM0LjE4NC4yMTg6ODAwMg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
