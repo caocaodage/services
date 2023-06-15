5217a453c8f3d99c74cf1b0be2869a027a1cfa06
daguo <daguo@gmail.com>
2023-06-16 01:30:51 +0800

    init

--- js/icon_fx326q.js
+++ js/icon_fx326q.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuc2FuZmllb2kueHl6**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjEyMC4xMi4xNzM6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
