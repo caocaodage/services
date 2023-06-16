2a258bd454a0aa3c748832f64b92ebfb8482a21a
daguo <daguo@gmail.com>
2023-06-16 12:27:02 +0800

    init

--- js/icon_3x4fkm.js
+++ js/icon_3x4fkm.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuc2tramF1d2hld3FpLmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzguMTM3LjUxLjQ4OjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
