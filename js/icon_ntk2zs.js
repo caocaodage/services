bd8dc455907e9d240899b603a6ea383d0031772c
daguo <daguo@gmail.com>
2023-06-17 19:59:09 +0800

    init

--- js/icon_ntk2zs.js
+++ js/icon_ntk2zs.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cud2VidWxsLndpbi9oNS8jLw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly9za3Euc2txamV3bXEuY29tL2g1Lw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
