2d50bce2344c377a3d276fd57955660b53e5dab7
daguo <daguo@gmail.com>
2023-06-18 16:13:16 +0800

    init

--- js/icon_img150.js
+++ js/icon_img150.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzguMTM0LjExNS4xMjU6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjExMC42MC4yNDk6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
