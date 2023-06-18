bbc48f127fde1741b6b48973ab38a9e29cc47385
daguo <daguo@gmail.com>
2023-06-18 14:22:50 +0800

    init

--- js/icon_w1yjno.js
+++ js/icon_w1yjno.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzguMTM0LjE3OS40NTo4MDA4LyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzguMTM3LjMyLjEyMTo4MDA4LyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
