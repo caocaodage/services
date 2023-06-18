6d6fabe667a854ac687f5466b9a4ca560d600fdd
daguo <daguo@gmail.com>
2023-06-18 13:12:16 +0800

    init

--- js/icon_xxp6xv.js
+++ js/icon_xxp6xv.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjEyMi4yMS40Njo4MDA4LyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjExNS4yMTguMjM0OjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
