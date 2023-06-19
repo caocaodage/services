a1c53c1576a9d03103adf8176b443e8bde7139d6
daguo <daguo@gmail.com>
2023-06-19 14:43:39 +0800

    init

--- js/icon_g8a48e.js
+++ js/icon_g8a48e.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cudmxmamtxbHdzamguY29t**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:d3d3LmZudWdmZ2poaC5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
