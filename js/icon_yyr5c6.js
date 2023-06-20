1699ce704fc6fe8dd28ca3c364a812e38ebd897c
daguo <daguo@gmail.com>
2023-06-20 16:05:22 +0800

    init

--- js/icon_yyr5c6.js
+++ js/icon_yyr5c6.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZmZlaXFlcXUuY29tL2luZGV4Lmh0bWw=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjExMC40OC4xNTk6ODAwOC9pbmRleC5odG1sIy9wYWdlcy9sb2dpbi9pbmRleA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
