1e18e696697e5af8d7492978f794e8ee6eebc4ac
daguo <daguo@gmail.com>
2023-06-24 00:43:26 +0800

    init

--- js/icon_3heht1.js
+++ js/icon_3heht1.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuY3ljcGF4b3MuY29tL2g1LyMv**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEzLjI1MC4xODUuMTIwOjg2ODYvaDUvIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
