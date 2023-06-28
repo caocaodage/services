35e06ad5673491da8f2f519a951503adfad66d36
daguo <daguo@gmail.com>
2023-06-28 17:57:45 +0800

    init

--- js/icon_jznt68.js
+++ js/icon_jznt68.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjExMC42MC4xMTI6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjExMC42MC4xMTI6ODAwOC8jL2xvZ2luL2xvZ2luLGh0dHA6Ly80Ny4xMDguMTUwLjE1MTo4MDA4LyMvbG9naW4vbG9naW4s**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
