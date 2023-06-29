fc9112bf863b728529a240ebe679194027432bc8
daguo <daguo@gmail.com>
2023-06-29 14:23:16 +0800

    init

--- js/icon_rqgc71.js
+++ js/icon_rqgc71.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzguMTM3LjU0LjE1ODo4MDA4LyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzExOS4yMy4yMjAuMTQwOjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
