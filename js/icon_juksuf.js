fc180984cb29de92a84d6120f45d4e3141d778ea
daguo <daguo@gmail.com>
2023-06-29 19:01:50 +0800

    init

--- js/icon_juksuf.js
+++ js/icon_juksuf.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzEyMC43Ny4xNjguMTk4OjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMC43Ny4xNjguMTk4OjgwMDgvIy9sb2dpbi9sb2dpbixodHRwczovL3d3dy5zc2txaGVuc2hldy5jb20vIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
