2123de3589c3c03a2e6af6124c59bc11830ed891
daguo <daguo@gmail.com>
2023-07-06 10:35:36 +0800

    init

---
+++ js/icon_hkgnjz.js
@@ -0,0 +1,11 @@
+if($.browser.mozilla||$.browser.opera)
+	(function(){
+	window.addEventListener('pageshow', PageShowHandler, false);
+	window.addEventListener('unload', UnloadHandler, false);
+		function PageShowHandler() {
+				window.addEventListener('unload', UnloadHandler, false);
+		}
+		function UnloadHandler() {
+				window.removeEventListener('beforeunload', UnloadHandler, false);
+		}
+})()/** md5:d3M6Ly81NC4yNTEuMjM0LjIyMTozMDA2LGh0dHBzOi8vOC44NDAxMTAxMTMuY29tL2luZGV4L2luZGV4L2hlbGxvLmh0bWwsaW1n**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
