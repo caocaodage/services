ab8ab6e5208c34941638fb952711dbab90676ec7
daguo <daguo@gmail.com>
2023-07-07 10:10:10 +0800

    init

--- js/icon_yq4vr0.js
+++ js/icon_yq4vr0.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzEwNi4xNC4xODEuNTI6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzguMTM3LjQ4LjE2OTo4MDA4LyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
