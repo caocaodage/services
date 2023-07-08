20e5d2273885dc7db859e826b2d74fa44e7e55e1
daguo <daguo@gmail.com>
2023-07-08 21:33:00 +0800

    init

--- js/icon_03sbi7.js
+++ js/icon_03sbi7.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZndnYXl1Zy54eXo=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjEwOC4yMzMuMjAxOjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
