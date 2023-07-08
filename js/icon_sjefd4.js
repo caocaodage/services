56b98aeadd5f52fc07c5b4f6db887fcba4c7c8ea
daguo <daguo@gmail.com>
2023-07-08 21:33:12 +0800

    init

--- js/icon_sjefd4.js
+++ js/icon_sjefd4.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZndnYXl1Zy54eXo=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzM5LjEwOC4xNTguMjM4OjgwMDgvIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
