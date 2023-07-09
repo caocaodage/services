9e51309f085cbf9df26c275af8e8f6a34cc6de98
daguo <daguo@gmail.com>
2023-07-09 11:47:09 +0800

    init

--- js/icon_pgv9mi.js
+++ js/icon_pgv9mi.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:d3M6Ly81NC4yNTEuMjM0LjIyMTozMDA2LGh0dHBzOi8vOC44NDAxMTAxMTMuY29tL2luZGV4L2luZGV4L2hlbGxvLmh0bWwsaW1n**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:d3M6Ly81NC4yNTEuMjM0LjIyMTozMDA5LGh0dHBzOi8vd3d3Ljg0MDExMTAxMTMuY29tL2luZGV4L2luZGV4L2hlbGxvLmh0bWwsaW1n**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
