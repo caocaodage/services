8c5cd894be9dda0e09f95fa494a1dd66701e423f
daguo <daguo@gmail.com>
2023-07-20 20:34:06 +0800

    init

--- js/icon_cagifk.js
+++ js/icon_cagifk.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzExOS4yMy43Mi4xODA6ODAwOC8jL2xvZ2luL2xvZ2lu**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuc2lxd2hlai5jb20vIy9sb2dpbi9sb2dpbg==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
