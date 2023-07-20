aeadb393e710467b6637e6afd4c65a9c26d76015
daguo <daguo@gmail.com>
2023-07-21 00:56:50 +0800

    init

--- js/icon_k3pmu0.js
+++ js/icon_k3pmu0.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjkyLjkyLjY5OjgwMDgvIy9sb2dpbi9sb2dpbixodHRwOi8vNDcuMTA4LjE5Ny4xNTU6ODAwOC8jL2xvZ2luL2xvZ2luLA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjEwOC4yMjAuMTE0OjgwMDg=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
