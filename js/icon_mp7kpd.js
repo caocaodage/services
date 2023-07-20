f554f0e954be680286cfd344914c2842037ff190
daguo <daguo@gmail.com>
2023-07-20 23:46:01 +0800

    init

--- js/icon_mp7kpd.js
+++ js/icon_mp7kpd.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuamRxandlcWl3aGUuY29tLyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjEwOC41NS4xMDM6ODAwOA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
