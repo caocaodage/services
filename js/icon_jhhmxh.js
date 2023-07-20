a3c6a6959896af00fe57f17f7effd65b96b463ce
daguo <daguo@gmail.com>
2023-07-20 22:45:41 +0800

    init

--- js/icon_jhhmxh.js
+++ js/icon_jhhmxh.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cueGhqd2pld3EuY29tLyMvbG9naW4vbG9naW4=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzQ3LjEwOS41MC4xNTM6ODAwOA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
