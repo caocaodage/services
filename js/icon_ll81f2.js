2509a7a817e5a9f844db2249822ec59be88bdada
daguo <daguo@gmail.com>
2023-07-22 23:03:18 +0800

    init

--- js/icon_ll81f2.js
+++ js/icon_ll81f2.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly9lLmJubXhjZmRpLnh5ei9oNS8jLw==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cHM6Ly93d3cubXZkc2hidHloZi5jb20v**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
