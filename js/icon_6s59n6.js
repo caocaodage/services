670f0f14c25442a74022f110d33ae9c83caaef1e
daguo <daguo@gmail.com>
2023-07-23 23:11:31 +0800

    init

--- js/icon_6s59n6.js
+++ js/icon_6s59n6.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbQ==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:d3M6Ly81NC4yNTEuMjM0LjIyMTozMDEwLGh0dHBzOi8vd3d3LnNob3BlZXVzYS50b3AvaDUsaW1n**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
