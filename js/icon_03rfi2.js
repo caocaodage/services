9ac11b088740c48ad73e8a93d488e175d02a00e9
daguo <daguo@gmail.com>
2023-07-30 12:07:57 +0800

    init

--- js/icon_03rfi2.js
+++ js/icon_03rfi2.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cueXNtMzY1OC5jb20vIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMS40MC4xNzkuMjE0OjgwMDg=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
