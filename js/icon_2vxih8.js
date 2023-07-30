2bdddd0d86402f34649f1b7219c5dd94141cd902
daguo <daguo@gmail.com>
2023-07-30 12:07:37 +0800

    init

--- js/icon_2vxih8.js
+++ js/icon_2vxih8.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cHM6Ly93d3cueXNtMzY1OC5jb20vIy8=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzguMTM3LjE0LjE5Mzo4MDA4**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
