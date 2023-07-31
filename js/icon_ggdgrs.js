912942727ee9a510bcb093bc273db9fd9d3875a3
daguo <daguo@gmail.com>
2023-07-31 16:07:02 +0800

    init

--- js/icon_ggdgrs.js
+++ js/icon_ggdgrs.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjEwOS43MS4yNDU6ODAwOA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMC43OS4yMzMuMTk1OjgwMDg=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
