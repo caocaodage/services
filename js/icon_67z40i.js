454c061593a0c8be7e331857d5e1032bf5aa0a5a
daguo <daguo@gmail.com>
2023-07-31 16:02:49 +0800

    init

--- js/icon_67z40i.js
+++ js/icon_67z40i.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzQ3LjEwOS43Ny42Njo4MDA4**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMS4xOTYuMjIwLjgzOjgwMDg=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
