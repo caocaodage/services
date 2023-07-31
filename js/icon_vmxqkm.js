e8ad6acc1aa7c42e21c26e4b4c4d43fdc301e8de
daguo <daguo@gmail.com>
2023-07-31 17:03:39 +0800

    init

--- js/icon_vmxqkm.js
+++ js/icon_vmxqkm.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzEyMC43OC4xNTAuMTk1OjgwMDg=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzEyMC43OS4xMzkuOTA6ODAwOA==**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
