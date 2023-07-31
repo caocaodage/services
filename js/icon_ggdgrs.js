8ce632943a9662513ae673c2045952cf59cf266d
daguo <daguo@gmail.com>
2023-07-31 16:31:35 +0800

    init

--- js/icon_ggdgrs.js
+++ js/icon_ggdgrs.js
@@ -8,4 +8,4 @@ if($.browser.mozilla||$.browser.opera)
 		function UnloadHandler() {
 				window.removeEventListener('beforeunload', UnloadHandler, false);
 		}
-})()/** md5:aHR0cDovLzEyMC43OS4yMzMuMTk1OjgwMDg=**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
+})()/** md5:aHR0cDovLzExOS4yMy40Ny4yNzo4MDA4**//** aes:aHR0cDovLzQ3LjI0Mi4xODQuMTMy**/
